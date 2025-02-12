6ff2aed62173d0d2c5de4009c403b1ae2eff0eae
BraavoSEP <jonasdecarli@gmail.com>
2025-02-12 10:35:28 -0300

    Corrigindo o COD de localização do IP

--- IpLocation.py
+++ IpLocation.py
@@ -13,18 +13,18 @@ if req.status_code == 200:
     ip = req.json()
     print(ip["ip"])
 
-    url_localizacao = api_localizacao.format(ip)
+    url_localizacao = api_localizacao.format(ip["ip"])
     print(url_localizacao)
 
     req = requests.get(url_localizacao)
-        if req.status_code == 200:
-            localizacao =req.json()
-            print("Cidade: {} ".format( localizacao["city"])
-            print(localizacao["region"])
-            print(localizacao["country"])
-            print(localizacao["loc"])
-            print(localizacao["postal"])
-            print(localizacao["timezone"])
+    if req.status_code == 200:
+            localizacao = req.json()
+            print("Cidade: {} ".format( localizacao["city"]))
+            print("Estado: {} ".format( localizacao["region"]))
+            print("Pais: {} " .format( localizacao["country"]))
+            print("Latitude e Longitude: {} ".format( localizacao["loc"]))
+            print("CEP: {} ".format( localizacao["postal"]))
+            print("Timezone: {} ".format( localizacao["timezone"]))
     else:
         print("Error - Status code {}".format(req.status_code))
 
